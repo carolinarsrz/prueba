477ea7337b9f0fc6dcee53b2f5d0b0646d101055
carolina <carolinarsrz@gmail.com>
2019-07-27 12:15:34 -0400

    linea en casa section

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,12 @@ $(function(){
   $("a").click(function(event){
     if (this.hash !=="") {
       event.preventDefault();
-
+      var navHeight = $("#navbar").height() * 2;
       var gato = this.hash;
-
-      $("html, body").animate({
-        scrollTop: $(gato).offset().top
-      }, 800, function(){
-        window.location.hash = gato;
-      });
+      $("html").animate({
+        scrollTop: $(gato).offset().top - navHeight
+      }, 800);
+      window.location.hash = gato;
     }
 });
 
