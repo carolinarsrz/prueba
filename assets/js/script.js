64c2d984eb138ff7bf53426514c540dfc6818e11
carolina <carolinarsrz@gmail.com>
2019-07-27 10:22:17 -0400

    trabajos - container fluid

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,24 @@ $(function(){
       var gato = this.hash;
 
       $("html, body").animate({
-        scrollTop: $(gato).offset().Top
+        scrollTop: $(gato).offset().top
       }, 800, function(){
         window.location.hash = gato;
       });
     }
+});
+
+  var qs, js, q, s, d = document,
+    gi = d.getElementById,
+    ce = d.createElement,
+    gt = d.getElementsByTagName,
+    id = "typef_orm",
+    b = "https://embed.typeform.com/";
+  if (!gi.call(d, id)) {
+    js = ce.call(d, "script");
+    js.id = id;
+    js.src = b + "embed.js";
+    q = gt.call(d, "script")[0];
+    q.parentNode.insertBefore(js, q)
+  }
 });
